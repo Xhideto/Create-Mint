8c09e4dcea27f3401c27fa5e3ef29a229fff9ec8
Dojima Shinji <100776572+Xhideto@users.noreply.github.com>
2024-10-15 10:40:46 +0800

    Update Batcave.sol

--- Batcave.sol
+++ Batcave.sol
@@ -4,31 +4,20 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
 
-contract BatCave is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
+contract BatCave is ERC20, ERC20Burnable, Ownable {
     constructor(address initialOwner)
         ERC20("BatCave", "BAT")
         Ownable(initialOwner)
     {}
 
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    function _update(address from, address to, uint256 value)
-        internal
-        override(ERC20, ERC20Pausable)
-    {
-        super._update(from, to, value);
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
     }
 }
